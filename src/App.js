423651b3fec65c805ef6fe408786cab66fbb5d40
Wahome <vinwahome@gmail.com>
2023-09-05 12:43:23 +0300

    Add the todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  function addTodo(todo) {
+    const newTodo = {
+      id: Math.random(),
+      todo : todo
+    }
+
+    // Add Todos to my list
+    setTodos([...todos, newTodo])
+
+    // Clear my List
+    setInput('')
+
+  }
+
+  function deleteTodo() {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='header'>My Todo List</h1>
+      <input 
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>
+        Add task
+      </button>
+      <ul>
+       {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.todo}
+          <button onClick={deleteTodo}>&times;</button>
+        </li>
+        
+       ))}
+      </ul>
     </div>
   );
 }
